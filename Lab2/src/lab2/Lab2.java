bb608053fcf329eb3b3b4b5187bc939b71e19b5c
Christopher B. Roberts <cbrobert@ncat.edu>
2018-08-27 15:53:22 -0400

    Make change to the code

--- Lab2/src/lab2/Lab2.java
+++ Lab2/src/lab2/Lab2.java
@@ -5,17 +5,55 @@
  */
 package lab2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
  *
  * @author cbrobert
  */
 public class Lab2 {
-
+static String [] records;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String filename;
+        String Firstname;
+        String Lastname;
+        String Gender;
+        String Age;
+        String PhoneNumber;
+        String EmailAddress;
+        System.out.println(records);
+  //Actions to perform if there is an exception
+}
+       // TODO code application logic here
+    
+    public static void readData (String filename){
+     records = new String[100];
+        try {
+ java.util.Scanner scanner = new java.util.Scanner (new File(String filename[0]));
+ int currentIndex = 0;
+while(scanner.hasNext()){
+    String[] line = scanner.nextLine().split(",");
+String Firstname = line[0];
+                String Lastname = line[1]; 
+                String Gender = line[2];
+                String Age = line[3];
+                String PhoneNumber = line[4];
+                String EmailAddress = line[5];
+                records[currentIndex] = String.format("String Firstname;\n" +
+"                String Lastname; \n" +
+"                String Gender;\n" +
+"                String Age;\n" +
+"                String PhoneNumber;\n" +
+"                String EmailAddress;");
+currentIndex++;//perform some operation that could cause an exception
+}
+}catch (FileNotFoundException ex) {
+        System.err.println("Failed to read input file");
+        }   
     }
     
 }
